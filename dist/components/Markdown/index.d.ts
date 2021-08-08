576c7d164772f8b498afb263bc3dc4563de19a80
Nick Beaulieu <beaulieun331@gmail.com>
2021-08-08 19:37:16 -0400

    remove markdown for size

--- dist/components/Markdown/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './styles.css';
-export interface MarkdownEditorProps {
-    className?: string;
-    value: string;
-    onChange(e: React.ChangeEvent<HTMLTextAreaElement>): void;
-    setValue?(value: React.SetStateAction<string>): () => void;
-}
-export declare const MarkdownEditor: React.FC<MarkdownEditorProps>;
